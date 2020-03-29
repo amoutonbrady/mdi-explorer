b66ee67cf2e90d7090026b98b116ca9ca498bac3
Alexandre <amoutonbrady@gmail.com>
2020-03-29 15:37:37 +0200

    :wrench: Fix PurgeCSS on dev

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,8 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 	defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 });
 
+const isProd = process.env.PARCEL_BUILD_ENV === 'production';
+
 module.exports = {
-	plugins: [require('tailwindcss'), purgecss],
+	plugins: [require('tailwindcss'), ...[isProd ? [purgecss] : []]],
 };
