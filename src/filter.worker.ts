14afc48198674b85eae0ded28074f41463241654
Alexandre <amoutonbrady@gmail.com>
2020-03-29 15:54:06 +0200

    :lipstick: Changed from 4 tabs to 2 spaces indendation
    Much more readable on GitHub

--- src/filter.worker.ts
+++ src/filter.worker.ts
@@ -1,29 +1,29 @@
 import * as Comlink from 'comlink';
 
-function filterFuckingThings(icons: string[][], search: string) {
-	const res = [];
+function filter(icons: string[][], search: string) {
+  const res = [];
 
-	for (let i = 0; i < icons.length; i++) {
-		const [name, path] = icons[i];
-		if (!name.toLocaleLowerCase().includes(search)) continue;
+  for (let i = 0; i < icons.length; i++) {
+    const [name, path] = icons[i];
+    if (!name.toLocaleLowerCase().includes(search)) continue;
 
-		res.push([
-			name,
-			path,
-			name
-				.slice(3)
-				.replace(
-					new RegExp(`(${search})`, 'gi'),
-					'<span class="border-b border-red-500">$1</span>',
-				),
-		]);
-	}
+    res.push([
+      name,
+      path,
+      name
+        .slice(3)
+        .replace(
+          new RegExp(`(${search})`, 'gi'),
+          '<span class="border-b border-red-300">$1</span>',
+        ),
+    ]);
+  }
 
-	return res;
+  return res;
 }
 
-Comlink.expose({ filterFuckingThings });
+Comlink.expose({ filter });
 
 export type WorkerFunction = {
-	filterFuckingThings: typeof filterFuckingThings;
+  filter: typeof filter;
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,125 +1,122 @@
 import * as Comlink from 'comlink';
 import { render, For } from 'solid-js/dom';
 import {
-	createState,
-	onError,
-	createEffect,
-	createResource,
-	createMemo,
+  createState,
+  onError,
+  createEffect,
+  createResource,
+  createMemo,
 } from 'solid-js';
 
 import { Icon } from './components/icon';
 import { WorkerFunction } from './filter.worker';
 
-type FuckingEvent = InputEvent & {
-	currentTarget: HTMLInputElement;
-	target: HTMLInputElement;
+type EventFromInput = InputEvent & {
+  currentTarget: HTMLInputElement;
+  target: HTMLInputElement;
 };
 
 function fetchIcons() {
-	return import('@mdi/js').then((module) => Object.entries(module));
+  return import('@mdi/js').then((module) => Object.entries(module));
 }
 
 function App() {
-	// STATE
-	let interval: number;
-	const [icons, loadIcons] = createResource([]);
-	const [state, setState] = createState({
-		search: '',
-		filteredIcons: [],
-		showCopiedText: false,
-	});
+  // STATE
+  let interval: number;
+  const [icons, loadIcons] = createResource([]);
+  const [state, setState] = createState({
+    search: '',
+    filteredIcons: [],
+    showCopiedText: false,
+  });
 
-	// INIT
-	const worker = Comlink.wrap<WorkerFunction>(
-		new Worker('./filter.worker.ts'),
-	);
-	loadIcons(fetchIcons());
+  // INIT
+  const worker = Comlink.wrap<WorkerFunction>(new Worker('./filter.worker.ts'));
+  loadIcons(fetchIcons());
 
-	// COMPUTED
-	const iconsLength = createMemo(() => icons().length);
+  // COMPUTED
+  const iconsLength = createMemo(() => icons().length);
 
-	// METHODS
-	function updateSearch({ target }: FuckingEvent) {
-		setState('search', target.value.toLocaleLowerCase());
-	}
+  // METHODS
+  function updateSearch({ target }: EventFromInput) {
+    setState('search', target.value.toLocaleLowerCase());
+  }
 
-	function copyToClipboard(iconName: string) {
-		navigator.clipboard.writeText(iconName).then(() => {
-			setState('showCopiedText', true);
-			setTimeout(() => setState('showCopiedText', false), 3000);
-		});
-	}
+  function copyToClipboard(iconName: string) {
+    navigator.clipboard.writeText(iconName).then(() => {
+      setState('showCopiedText', true);
+      setTimeout(() => setState('showCopiedText', false), 3000);
+    });
+  }
 
-	createEffect(() => {
-		const now = performance.now();
+  // WATCHERS
+  createEffect(() => {
+    const now = performance.now();
 
-		worker
-			.filterFuckingThings(icons(), state.search)
-			.then((filtered: string[][]) => {
-				const copy = [...filtered];
-				clearInterval(interval);
-				setState('filteredIcons', []);
+    worker.filter(icons(), state.search).then((filtered: string[][]) => {
+      const copy = [...filtered];
+      clearInterval(interval);
+      setState('filteredIcons', []);
 
-				interval = setInterval(() => {
-					if (!copy.length) return clearInterval(interval);
+      interval = setInterval(() => {
+        if (!copy.length) return clearInterval(interval);
 
-					setState('filteredIcons', [
-						...state.filteredIcons,
-						...copy.splice(0, 500),
-					]);
-				}, 150);
+        setState('filteredIcons', [
+          ...state.filteredIcons,
+          ...copy.splice(0, 500),
+        ]);
+      }, 150);
 
-				const then = performance.now();
-				console.log(`Filter took ${then - now}ms`);
-			});
-	});
+      const then = performance.now();
+      console.log(`Filter took ${then - now}ms`);
+    });
+  });
 
-	onError(console.error);
+  onError(console.error);
 
-	return (
-		<main class="container p-6 mx-auto relative">
-			<h1 class="text-center text-4xl font-semibold">MDI Explorer</h1>
+  return (
+    <main class="container p-6 mx-auto relative">
+      <h1 class="text-center text-4xl font-semibold">MDI Explorer</h1>
 
-			<input
-				onInput={updateSearch}
-				value={state.search}
-				class="uppercase border border-gray-700 bg-transparent rounded px-4 py-2 w-full mt-6 bg-gray-800 top-6 sticky z-20"
-				placeholder="Search an icon..."
-			/>
+      <input
+        onInput={updateSearch}
+        value={state.search}
+        class="uppercase border border-gray-700 bg-transparent rounded px-4 py-2 w-full mt-6 bg-gray-800 top-6 sticky z-20"
+        placeholder="Search an icon..."
+      />
 
-			<p class="mt-3 text-right font-semibold">
-				{state.filteredIcons.length}/{iconsLength}
-			</p>
+      <p class="mt-3 text-right font-semibold">
+        {state.filteredIcons.length}/{iconsLength()}
+      </p>
 
-			<section class="grid grid-cols-5 sm:grid-cols-8 md:grid-cols-12 lg:grid-cols-15 gap-6 mt-12 relative z-10">
-				<For each={state.filteredIcons}>
-					{([id, path, name]) => (
-						<button
-							onClick={[copyToClipboard, id]}
-							class="relative group border border-transparent hover:border-gray-500 rounded p-1"
-						>
-							<Icon path={path} id={id} class="w-full" />
-							<p
-								innerHTML={name}
-								class="text-sm font-semibold absolute  z-20 top-full left-1/2 -translate-x-1/2 transform translate-y-2 bg-gray-600 rounded shadow border-gray-900 px-3 py-2 group-hover:block group-active:block hidden"
-							></p>
-						</button>
-					)}
-				</For>
-			</section>
+      <section class="grid grid-cols-5 sm:grid-cols-8 md:grid-cols-12 lg:grid-cols-15 gap-6 mt-12 relative z-10">
+        <For each={state.filteredIcons}>
+          {([id, path, name]) => (
+            <button
+              onClick={[copyToClipboard, id]}
+              class="relative group border border-transparent hover:border-gray-500 rounded p-1"
+            >
+              <Icon path={path} id={id} class="w-full" />
+              <p
+                innerHTML={name}
+                class="text-sm font-semibold absolute  z-20 top-full left-1/2 -translate-x-1/2 transform translate-y-2 bg-gray-600 rounded shadow border-gray-900 px-3 py-2 group-hover:block group-active:block hidden"
+              ></p>
+            </button>
+          )}
+        </For>
+      </section>
 
-			<p
-				class="absolute top-6 right-6 px-3 py-2 font-semibold bg-green-200 text-green-800 border border-green-800 rounded shadow transition duration-300 transform z-30"
-				classList={{
-					'scale-1 opacity-1': state.showCopiedText,
-					'scale-0 opacity-0': !state.showCopiedText,
-				}}
-			>
-				Icon copied to clipboard!
-			</p>
-		</main>
-	);
+      <p
+        class="absolute top-6 right-6 px-3 py-2 font-semibold bg-green-200 text-green-800 border border-green-800 rounded shadow transition duration-300 transform z-30"
+        classList={{
+          'scale-1 opacity-1': state.showCopiedText,
+          'scale-0 opacity-0': !state.showCopiedText,
+        }}
+      >
+        Icon copied to clipboard!
+      </p>
+    </main>
+  );
 }
 
 render(App, document.getElementById('app'));
