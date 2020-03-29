444579d58a6702f40bc859d6a09d3eba8bcd4c25
Alexandre <amoutonbrady@gmail.com>
2020-03-29 22:20:20 +0200

    :sparkles: Add a way to download SVG + loader

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import * as Comlink from 'comlink';
-import { render, For } from 'solid-js/dom';
+import { render, For, Switch, Match } from 'solid-js/dom';
 import {
   createState,
   onError,
@@ -22,43 +22,73 @@ function fetchIcons() {
 }
 
 function App() {
+  // -----
   // STATE
+  // -----
   const [icons, loadIcons] = createResource([]);
   const [state, setState] = createState({
     search: '',
-    filteredIcons: [] as string[][],
-    filteredLength: 0,
-    showCopiedText: false,
-    perPage: 100,
+    loading: false,
     page: 0,
+    perPage: 100,
     workerReady: false,
+    showCopiedText: false,
+    filteredLength: 0,
+    filteredIcons: [] as string[][],
   });
+  let loading = () => false;
 
+  // ----
   // INIT
+  // ----
   const worker = Comlink.wrap<MDIWorker>(new Worker('./filter.worker.ts'));
 
-  loadIcons(fetchIcons());
-
+  // --------
   // COMPUTED
+  // --------
   const numberOfPages = createMemo(() =>
     Math.ceil(state.filteredLength / state.perPage),
   );
 
+  // -------
   // METHODS
+  // -------
   function updateSearch({ target }: EventFromInput) {
-    setState('search', target.value.toLocaleLowerCase());
+    setState((state) => {
+      state.search = target.value.toLocaleLowerCase();
+      state.page = 0;
+    });
   }
 
-  function copyToClipboard({ iconName = '', duration = 3000 }) {
-    if (!iconName) return;
+  function copyToClipboard({ id = '', duration = 3000 }) {
+    if (!id) return;
 
-    navigator.clipboard.writeText(iconName).then(() => {
+    navigator.clipboard.writeText(id).then(() => {
       setState('showCopiedText', true);
       setTimeout(() => setState('showCopiedText', false), duration);
     });
   }
 
+  function downloadSVG({ id = '', path = '', duration = 3000 }) {
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24"><path d="${path}" /></svg>`;
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(
+      new Blob([svg], { type: 'text/svg;charset=utf-8;' }),
+    );
+    a.download = `${id}.svg`;
+
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    copyToClipboard({ id, duration });
+  }
+
+  // --------
   // WATCHERS
+  // --------
+  createEffect(() => (loading = loadIcons(fetchIcons())));
+
   createEffect(() => {
     if (!icons().length) return;
 
@@ -88,75 +118,103 @@ function App() {
     <main class="container p-6 mx-auto relative">
       <h1 class="text-center text-4xl font-semibold">MDI Explorer</h1>
 
-      <input
-        value={state.search}
-        onInput={updateSearch}
-        class="uppercase border border-gray-700 bg-transparent rounded px-4 py-2 w-full mt-6 bg-gray-800 top-6 sticky z-20"
-        placeholder="Search an icon..."
-      />
-
-      <p class="mt-3 text-right font-semibold">
-        {state.filteredLength} result(s)
-      </p>
-
-      <section class="grid grid-cols-5 sm:grid-cols-8 md:grid-cols-12 lg:grid-cols-15 gap-6 mt-12 relative z-10">
-        <For each={state.filteredIcons}>
-          {([id, path, name]) => (
-            <button
-              onClick={[copyToClipboard, { iconName: id }]}
-              class="relative group border border-transparent hover:border-gray-500 rounded p-1"
-            >
-              <Icon path={path} id={id} class="w-full" />
-              <p
-                innerHTML={name}
-                class="text-sm font-semibold absolute z-20 top-full left-1/2 -translate-x-1/2 transform translate-y-2 bg-gray-600 rounded shadow border-gray-900 px-3 py-2 group-hover:block group-active:block hidden"
-              ></p>
-            </button>
-          )}
-        </For>
-      </section>
-
-      <div class="flex justify-between mt-20">
-        <button
-          onClick={() => setState('page', state.page - 1)}
-          disabled={state.page === 0}
-          classList={{ 'opacity-0 scale-0': state.page === 0 }}
-          class="bg-gray-800 px-3 py-2 rounded border border-gray-700 hover:bg-gray-900 active:bg-black text-sm font-mono uppercase tracking-wide transition transform duration-300"
-        >
-          <span>&lt; Previous page</span>
-        </button>
-
-        <div
-          class="text-xl font-mono flex items-center scale-1 transform transition duration-300"
-          classList={{ 'opacity-0 scale-0': numberOfPages() === 0 }}
-        >
+      <Switch
+        fallback={
+          <span class="text-red-500 text-xl mt-12">An error occured...</span>
+        }
+      >
+        <Match when={loading()}>
+          <p class="mt-12 text-xl text-center">Loading the icons...</p>
+        </Match>
+        <Match when={!loading()}>
           <input
-            type="number"
-            name="pagination"
-            id="pagination"
-            min="1"
-            max={numberOfPages()}
-            value={state.page + 1}
-            onInput={(e) =>
-              setState('page', Number.parseInt(e.target.value, 10) - 1)
-            }
-            class="border border-transparent hover:border-gray-700 bg-transparent rounded px-4 py-2 bg-transparent w-20 relative transform translate-x-12 hover:translate-x-0 transition duration-300"
+            value={state.search}
+            onInput={updateSearch}
+            class="uppercase border border-gray-700 bg-transparent rounded px-4 py-2 w-full mt-6 bg-gray-800 top-6 sticky z-20"
+            placeholder="Search an icon..."
           />
-          <span>&nbsp;of&nbsp;</span>
-          <span>{numberOfPages()}</span>
-        </div>
 
-        <button
-          onClick={() => setState('page', state.page + 1)}
-          disabled={state.filteredIcons.length < state.perPage}
-          classList={{
-            'opacity-0 scale-0': state.filteredIcons.length < state.perPage,
-          }}
-          class="bg-gray-800 px-3 py-2 rounded border border-gray-700 hover:bg-gray-900 active:bg-black text-sm font-mono uppercase tracking-wide transition transform duration-300"
-        >
-          <span>Next page &gt;</span>
-        </button>
-      </div>
+          <p class="mt-3 text-right font-semibold">
+            {state.filteredLength} result(s)
+          </p>
+
+          <section class="grid grid-cols-5 sm:grid-cols-8 md:grid-cols-12 lg:grid-cols-15 gap-6 mt-12 relative z-10">
+            <For each={state.filteredIcons}>
+              {([id, path, name]) => (
+                <a
+                  href={`#/${id}`}
+                  class="relative group border border-transparent hover:border-gray-500 rounded p-1"
+                >
+                  <Icon path={path} id={id} class="w-full" />
+
+                  <div class="text-sm overflow-hidden font-semibold absolute z-20 top-full left-1/2 -translate-x-1/2 transform bg-gray-600 rounded shadow border-gray-900 group-hover:block group-focus:block group-active:block hidden">
+                    <p innerHTML={name} class="p-2 text-center"></p>
+
+                    <div class="flex">
+                      <button
+                        onClick={[downloadSVG, { id, path }]}
+                        class="flex-1 w-full border-t-2 border-r border-gray-400 p-1 px-6 hover:bg-gray-700 active:bg-gray-800"
+                      >
+                        SVG
+                      </button>
+                      <button
+                        onClick={[copyToClipboard, { id }]}
+                        class="flex-1 w-full border-t-2 border-l border-gray-400 p-1 px-6 hover:bg-gray-700 active:bg-gray-800"
+                      >
+                        ID
+                      </button>
+                    </div>
+                  </div>
+                </a>
+              )}
+            </For>
+          </section>
+
+          <div class="flex justify-between mt-20">
+            <button
+              onClick={() => setState('page', state.page - 1)}
+              disabled={state.page === 0}
+              classList={{ 'opacity-0 scale-0': state.page === 0 }}
+              class="bg-gray-800 px-3 py-2 rounded border border-gray-700 hover:bg-gray-900 active:bg-black text-sm font-mono uppercase tracking-wide transition transform duration-300"
+            >
+              <span>&lt;</span>
+              <span class="hidden sm:inline-block">&nbsp;Previous page</span>
+            </button>
+
+            <div
+              class="text-xl font-mono flex items-center scale-1 transform transition duration-300"
+              classList={{ 'opacity-0 scale-0': numberOfPages() === 0 }}
+            >
+              <input
+                type="number"
+                name="pagination"
+                id="pagination"
+                min="1"
+                max={numberOfPages()}
+                value={state.page + 1}
+                onInput={(e) =>
+                  setState('page', Number.parseInt(e.target.value, 10) - 1)
+                }
+                class="border border-transparent hover:border-gray-700 bg-transparent rounded px-4 py-2 bg-transparent w-20 relative transform translate-x-12 hover:translate-x-0 transition duration-300"
+              />
+              <span>&nbsp;of&nbsp;</span>
+              <span>{numberOfPages()}</span>
+            </div>
+
+            <button
+              onClick={() => setState('page', state.page + 1)}
+              disabled={state.filteredIcons.length < state.perPage}
+              classList={{
+                'opacity-0 scale-0': state.filteredIcons.length < state.perPage,
+              }}
+              class="bg-gray-800 px-3 py-2 rounded border border-gray-700 hover:bg-gray-900 active:bg-black text-sm font-mono uppercase tracking-wide transition transform duration-300"
+            >
+              <span class="hidden sm:inline-block">Next page&nbsp;</span>
+              <span>&gt;</span>
+            </button>
+          </div>
+        </Match>
+      </Switch>
 
       <Notification show={state.showCopiedText} />
     </main>
